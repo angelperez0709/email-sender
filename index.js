a8bd4ab01ba86b1f6d9b15f5f156b37b9c4efbc9
angelperez0709 <angelperez07092000@gmai.com>
2024-02-12 12:18:56 +0100

    change port

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.post('/', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+app.listen(443, () => {
+  console.log('Server is listening on port 443');
 });
